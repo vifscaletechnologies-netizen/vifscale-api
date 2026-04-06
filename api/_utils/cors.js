87f81234b5c20cba422a9332af3753811f0d6c28
vifscaletechnologies-netizen <vifscaletechnologies@gmail.com>
2026-04-06 20:15:22 +0100

    Update cors.js

--- api/_utils/cors.js
+++ api/_utils/cors.js
@@ -1,1 +1,13 @@
 
+export const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'POST, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Accept'
+};
+
+export function handleCors(res) {
+  Object.entries(corsHeaders).forEach(([key, value]) => {
+    res.setHeader(key, value);
+  });
+  return res.status(200).end();
+}
